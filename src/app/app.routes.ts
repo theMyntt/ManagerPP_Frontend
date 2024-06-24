fb74a98e1363709d13c83699981551fec824f739
Gabriel Araújo Lima <gabriel.araujo2902@outlook.com>
2024-06-23 22:30:53 -0300

    ci: providing login route to router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router'
+import { AccessComponent } from '@features/access/access.component'
 
-export const routes: Routes = []
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: AccessComponent
+  }
+]
